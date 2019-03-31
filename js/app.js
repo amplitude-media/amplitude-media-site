d2a9393057c1595b4168550dc524338d09f77bec
Reid <reid@amplitude.media>
2019-03-31 15:10:53 -0700

    new site

---
+++ js/app.js
@@ -0,0 +1,65 @@
+//var TIMEOUT = 0;
+//this controls the time between slides //
+
+
+var interval = setInterval(handleNext, TIMEOUT);
+
+function handleNext() {
+
+  var $radios = $('input[class*="slide-radio"]');
+  var $activeRadio = $('input[class*="slide-radio"]:checked');
+
+  var currentIndex = $activeRadio.index();
+  var radiosLength = $radios.length;
+
+  $radios
+    .attr('checked', false);
+
+
+  if (currentIndex >= radiosLength - 1) {
+
+    $radios
+      .first()
+      .attr('checked', true);
+
+  } else {
+
+    $activeRadio
+      .next('input[class*="slide-radio"]')
+      .attr('checked', true);
+
+  }
+
+}
+
+
+
+/*
+function handleNext() {
+
+  var $radios = $('input[class*="slide-radio"]');
+  var $activeRadio = $('input[class*="slide-radio"]:checked');
+
+  var currentIndex = $activeRadio.index();
+  var radiosLength = $radios.length;
+
+  $radios
+    .attr('checked', false);
+
+
+  if (currentIndex >= radiosLength - 1) {
+
+    $radios
+      .first()
+      .attr('checked', true);
+
+  } else {
+
+    $activeRadio
+      .next('input[class*="slide-radio"]')
+      .attr('checked', true);
+
+  }
+
+}
+*/
